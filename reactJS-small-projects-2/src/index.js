ede46007aa34fddac60a3835b410424a41bc9ec9
bayar <bayar@nest-33s-iMac.local>
2021-11-10 11:16:41 +0800

    remove snake duplicate

--- reactJS-small-projects-2/src/index.js
+++ reactJS-small-projects-2/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './routeApp';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 // import Counter from './mount';
 // import Todo from './todoFunction';
 // import reportWebVitals from './reportWebVitals';
@@ -11,14 +11,10 @@ ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals(console.log);
-
-
--- reactJS-small-projects-2/src/nodeSnake.js
+++ reactJS-small-projects-2/src/nodeSnake.js
@@ -1,140 +1,144 @@
-const chalk = require('chalk');
+const chalk = require("chalk");
 
 const stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.resume();
-stdin.setEncoding('utf8');
+stdin.setEncoding("utf8");
 
-const KEY_UP = '\u001B\u005B\u0041';
-const KEY_RIGHT = '\u001B\u005B\u0043';
-const KEY_DOWN = '\u001B\u005B\u0042';
-const KEY_LEFT = '\u001B\u005B\u0044';
-const KEY_CTRL_C = '\u0003';
+const KEY_UP = "\u001B\u005B\u0041";
+const KEY_RIGHT = "\u001B\u005B\u0043";
+const KEY_DOWN = "\u001B\u005B\u0042";
+const KEY_LEFT = "\u001B\u005B\u0044";
+const KEY_CTRL_C = "\u0003";
 
-const BOARD_CHAR = '\u00b7';
+const BOARD_CHAR = "\u00b7";
 const BOARD_WIDTH = 20;
 const BOARD_HEIGHT = 10;
-const BOARD_TITLE = 'SNAKE';
+const BOARD_TITLE = "SNAKE";
 
 const DIRECTIONS = {
-    UP: 'up',
-    RIGHT: 'right',
-    DOWN: 'down',
-    LEFT: 'left',
+  UP: "up",
+  RIGHT: "right",
+  DOWN: "down",
+  LEFT: "left",
 };
 
-const SNAKE_CHAR = '\u25aa'; // \u25ab
+const SNAKE_CHAR = "\u25aa"; // \u25ab
 const SNAKE = [
-    { x: 15, y: 5 },
-    { x: 16, y: 5 },
-    { x: 17, y: 5 },
+  { x: 15, y: 5 },
+  { x: 16, y: 5 },
+  { x: 17, y: 5 },
 ];
 let SNAKE_DIRECTION = DIRECTIONS.DOWN;
 
-const APPLE_CHAR = '\uf8ff';
+const APPLE_CHAR = "\uf8ff";
 let APPLE_Y = 5;
 let APPLE_X = 5;
 
 const board = [];
-const column = BOARD_CHAR.repeat(BOARD_WIDTH).split('');
-for (let i = 0; i < BOARD_HEIGHT; i ++) {
-    board.push(column);
+const column = BOARD_CHAR.repeat(BOARD_WIDTH).split("");
+for (let i = 0; i < BOARD_HEIGHT; i++) {
+  board.push(column);
 }
 
 const getRandomCoord = () => {
-    const x = Math.round(Math.random() * (BOARD_WIDTH - 1));
-    const y = Math.round(Math.random() * (BOARD_HEIGHT - 1));
-    return [x, y];
-}
+  const x = Math.round(Math.random() * (BOARD_WIDTH - 1));
+  const y = Math.round(Math.random() * (BOARD_HEIGHT - 1));
+  return [x, y];
+};
 
 // Reading key inputs
-stdin.on('data', (key) => {
-    switch (key) {
-        case KEY_UP:
-            SNAKE_DIRECTION = DIRECTIONS.UP;
-            break;
-        case KEY_RIGHT:
-            SNAKE_DIRECTION = DIRECTIONS.RIGHT;
-            break;
-        case KEY_DOWN:
-            SNAKE_DIRECTION = DIRECTIONS.DOWN;
-            break;
-        case KEY_LEFT:
-            SNAKE_DIRECTION = DIRECTIONS.LEFT;
-            break;
-        case KEY_CTRL_C:
-            process.exit();
-            break;
-    }
+stdin.on("data", (key) => {
+  switch (key) {
+    case KEY_UP:
+      SNAKE_DIRECTION = DIRECTIONS.UP;
+      break;
+    case KEY_RIGHT:
+      SNAKE_DIRECTION = DIRECTIONS.RIGHT;
+      break;
+    case KEY_DOWN:
+      SNAKE_DIRECTION = DIRECTIONS.DOWN;
+      break;
+    case KEY_LEFT:
+      SNAKE_DIRECTION = DIRECTIONS.LEFT;
+      break;
+    case KEY_CTRL_C:
+      process.exit();
+      break;
+  }
 });
 
 setInterval(() => {
-    console.clear();
+  console.clear();
 
-    const headerPadStart = (BOARD_WIDTH + BOARD_TITLE.length) / 2;
-    console.log(chalk.green(BOARD_TITLE.padStart(headerPadStart)));
+  const headerPadStart = (BOARD_WIDTH + BOARD_TITLE.length) / 2;
+  console.log(chalk.green(BOARD_TITLE.padStart(headerPadStart)));
 
-    switch (SNAKE_DIRECTION) {
-        case DIRECTIONS.UP:
-            SNAKE.unshift({
-                x: SNAKE[0].x,
-                y: SNAKE[0].y - 1,
-            });
-            break;
-        case DIRECTIONS.RIGHT:
-            SNAKE.unshift({
-                x: SNAKE[0].x + 1,
-                y: SNAKE[0].y,
-            });
-            break;
-        case DIRECTIONS.DOWN:
-            SNAKE.unshift({
-                x: SNAKE[0].x,
-                y: SNAKE[0].y + 1,
-            });
-            break;
-        case DIRECTIONS.LEFT:
-            SNAKE.unshift({
-                x: SNAKE[0].x - 1,
-                y: SNAKE[0].y,
-            });
-            break;
-    }
-    if (SNAKE[0].y === APPLE_Y && SNAKE[0].x === APPLE_X) {
-        const coords = getRandomCoord();
-        APPLE_X = coords[0];
-        APPLE_Y = coords[1];
-    } else {
-        SNAKE.pop();
-    }
+  switch (SNAKE_DIRECTION) {
+    case DIRECTIONS.UP:
+      SNAKE.unshift({
+        x: SNAKE[0].x,
+        y: SNAKE[0].y - 1,
+      });
+      break;
+    case DIRECTIONS.RIGHT:
+      SNAKE.unshift({
+        x: SNAKE[0].x + 1,
+        y: SNAKE[0].y,
+      });
+      break;
+    case DIRECTIONS.DOWN:
+      SNAKE.unshift({
+        x: SNAKE[0].x,
+        y: SNAKE[0].y + 1,
+      });
+      break;
+    case DIRECTIONS.LEFT:
+      SNAKE.unshift({
+        x: SNAKE[0].x - 1,
+        y: SNAKE[0].y,
+      });
+      break;
+  }
+  if (SNAKE[0].y === APPLE_Y && SNAKE[0].x === APPLE_X) {
+    const coords = getRandomCoord();
+    APPLE_X = coords[0];
+    APPLE_Y = coords[1];
+  } else {
+    SNAKE.pop();
+  }
 
-    if (SNAKE[0].x < 0)                SNAKE[0].x = BOARD_WIDTH - 1;
-    if (SNAKE[0].x > BOARD_WIDTH - 1)  SNAKE[0].x = 0;
-    if (SNAKE[0].y < 0)                SNAKE[0].y = BOARD_HEIGHT - 1;
-    if (SNAKE[0].y > BOARD_HEIGHT - 1) SNAKE[0].y = 0;
+  if (SNAKE[0].x < 0) SNAKE[0].x = BOARD_WIDTH - 1;
+  if (SNAKE[0].x > BOARD_WIDTH - 1) SNAKE[0].x = 0;
+  if (SNAKE[0].y < 0) SNAKE[0].y = BOARD_HEIGHT - 1;
+  if (SNAKE[0].y > BOARD_HEIGHT - 1) SNAKE[0].y = 0;
 
-    board.map((col, y) => {
-        return col.map((char, x) => {
-            const isSnakeBody = SNAKE.find((coord) => {
-                return coord.x === x && coord.y === y;
-            });
-            if (isSnakeBody) {
-                return SNAKE_CHAR;
-            }
-
-            if (y === APPLE_Y && x === APPLE_X) {
-                return APPLE_CHAR;
-            }
-            return char;
+  board
+    .map((col, y) => {
+      return col.map((char, x) => {
+        const isSnakeBody = SNAKE.find((coord) => {
+          return coord.x === x && coord.y === y;
         });
-    }).forEach((col) => {
-        console.log(col.join(''));
+        if (isSnakeBody) {
+          return SNAKE_CHAR;
+        }
+
+        if (y === APPLE_Y && x === APPLE_X) {
+          return APPLE_CHAR;
+        }
+        return char;
+      });
+    })
+    .forEach((col) => {
+      console.log(col.join(""));
     });
 
-    const isDead = SNAKE.find((coord, index) =>
-        index !== 0 && coord.x === SNAKE[0].x && coord.y === SNAKE[0].y)
-    if (isDead) {
-        console.log('GAME OVER!');
-        process.exit();
-    }
+  const isDead = SNAKE.find(
+    (coord, index) =>
+      index !== 0 && coord.x === SNAKE[0].x && coord.y === SNAKE[0].y
+  );
+  if (isDead) {
+    console.log("GAME OVER!");
+    process.exit();
+  }
 }, 300);
